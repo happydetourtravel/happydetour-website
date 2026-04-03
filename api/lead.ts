3652dd3527569696be05d4351f9408131bb7382c
happydetourtravel <happydetourtravel@gmail.com>
2026-04-02 20:11:57 -0400

    Delete api directory

--- api/lead.ts
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const data = req.body;
-    console.log('New Lead:', data);
-    return res.status(200).json({ message: 'Lead received' });
-  }
-
-  return res.status(405).json({ message: 'Method not allowed' });
-}
