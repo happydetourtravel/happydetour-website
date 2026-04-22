9229bb4d9bd4d56e72d776d18d03058f2d071a7c
happydetourtravel <happydetourtravel@gmail.com>
2026-04-22 01:27:38 -0400

    Update HeroSection.tsx

--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -8,6 +8,8 @@ const HERO_IMAGE =
 const FORM_URL = "https://happydetour.app.n8n.cloud/form/91799171-4079-4ab7-9d13-9417ae57dfba";
 
 export default function HeroSection() {
+  const [showModal, setShowModal] = React.useState(false);
+  return (
   return (
     <section
       id="hero"
