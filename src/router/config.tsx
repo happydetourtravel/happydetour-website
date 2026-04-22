2097b37fed061f4928a1339c8794a17f9bbe9ff4
happydetourtravel <happydetourtravel@gmail.com>
2026-04-22 00:55:23 -0400

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,11 +1,11 @@
 import type { RouteObject } from "react-router-dom";
 import NotFound from "../pages/NotFound";
-import Home from "../pages/home/page";
+import Success from "../pages/success/page";
 
 const routes: RouteObject[] = [
   {
-    path: "/",
-    element: <Home />,
+    path: "/success",
+    element: <Success />,
   },
   {
     path: "*",
