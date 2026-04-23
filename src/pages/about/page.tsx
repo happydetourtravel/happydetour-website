1a5c9d128fe92485100240711e77899101b96a3a
happydetourtravel <happydetourtravel@gmail.com>
2026-04-22 21:15:57 -0400

    Update page.tsx

--- src/pages/about/page.tsx
+++ src/pages/about/page.tsx
@@ -1,13 +1,4 @@
-import { useEffect } from "react";
-
 export default function About() {
-  useEffect(() => {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = "https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap";
-    document.head.appendChild(link);
-  }, []);
-
   return (
     <iframe
       src="/about.html"
