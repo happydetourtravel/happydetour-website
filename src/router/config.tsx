cd264fccf591147ce279ac472f8032226d327952
happydetourtravel <happydetourtravel@gmail.com>
2026-04-25 20:48:12 -0400

    Create config.tsx

---
+++ src/router/config.tsx
@@ -0,0 +1,26 @@
+import type { RouteObject } from "react-router-dom";
+import NotFound from "../pages/NotFound";
+import Home from "../pages/home/page";
+import Success from "../pages/success/page";
+import Testimonials from "../pages/testimonials/page";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/success",
+    element: <Success />,
+  },
+  {
+    path: "/testimonials",
+    element: <Testimonials />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
+
+export default routes;
