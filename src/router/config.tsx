959e5e8eb742aa1631575ac55f6ffa7ba7a6abd8
happydetourtravel <happydetourtravel@gmail.com>
2026-04-28 23:53:47 -0400

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -6,6 +6,8 @@ import Testimonials from "../pages/testimonials/page";
 import ReviewSuccess from "../pages/review-success/page";
 import Destinations from "../pages/destinations/page";
 import EveryoneWelcome from "../pages/everyone-welcome/page";
+import EveryoneWelcome from "../pages/everyone-welcome";
+
 const routes: RouteObject[] = [
   {
     path: "/",
