794300c5597a1058c7a26b334255459a39406a6f
happydetourtravel <happydetourtravel@gmail.com>
2026-04-28 23:32:58 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,20 +2,16 @@ import { useNavigate, type NavigateFunction } from "react-router-dom";
 import { useRoutes } from "react-router-dom";
 import { useEffect } from "react";
 import routes from "./config";
-import EveryoneWelcome from './EveryoneWelcome';
 
 let navigateResolver: (navigate: ReturnType<typeof useNavigate>) => void;
-
 declare global {
   interface Window {
     REACT_APP_NAVIGATE: ReturnType<typeof useNavigate>;
   }
 }
-
 export const navigatePromise = new Promise<NavigateFunction>((resolve) => {
   navigateResolver = resolve;
 });
-
 export function AppRoutes() {
   const element = useRoutes(routes);
   const navigate = useNavigate();
