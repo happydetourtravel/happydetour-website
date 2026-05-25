4021a5926716b74931dfa9f9e1fcbf552e9ecdcc
happydetourtravel <happydetourtravel@gmail.com>
2026-05-24 22:10:26 -0400

    Update FAQSection.tsx

--- src/pages/home/components/FAQSection.tsx
+++ src/pages/home/components/FAQSection.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import ScrollReveal from '../../../components/base/ScrollReveal';
 
-const FORM_URL = "https://happydetour.app.n8n.cloud/form/91799171-4079-4ab7-9d13-9417ae57dfba";
+const FORM_URL = "https://api.leadconnectorhq.com/widget/form/V4qo3lEB8oTtXbuWILfK";
 
 interface FAQ {
   question: string;
