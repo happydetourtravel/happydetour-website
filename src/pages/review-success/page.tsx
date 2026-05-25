4c0f32f7ad3b377b85f47f4568d91ffd4c698eb8
happydetourtravel <happydetourtravel@gmail.com>
2026-05-25 16:55:05 -0400

    Update FORM_URL to new API endpoint

--- src/pages/review-success/page.tsx
+++ src/pages/review-success/page.tsx
@@ -1,6 +1,6 @@
 import ScrollReveal from '../../components/base/ScrollReveal';
 
-const FORM_URL = "https://happydetour.app.n8n.cloud/form/91799171-4079-4ab7-9d13-9417ae57dfba";
+const FORM_URL = "https://api.leadconnectorhq.com/widget/form/V4qo3lEB8oTtXbuWILfK";
 const BLOG_URL = "https://happydetourtravel.wordpress.com/";
 const INSTAGRAM_URL = "https://www.instagram.com/happydetourtravel";
 const FACEBOOK_URL = "https://www.facebook.com/profile.php?id=61582209485454";
