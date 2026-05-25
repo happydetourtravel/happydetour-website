62cf66331e55c8b172bb264b4885582d95f11a0d
happydetourtravel <happydetourtravel@gmail.com>
2026-05-24 22:49:29 -0400

    Update NavBar.tsx

--- src/pages/home/components/NavBar.tsx
+++ src/pages/home/components/NavBar.tsx
@@ -49,7 +49,7 @@ export default function NavBar() {
 
         <nav className="relative mx-auto flex w-full max-w-7xl items-center justify-between px-6 py-2.5 lg:px-10 lg:py-3">
           {/* Logo */}
-          
+          <a
             href="#hero"
             className="flex items-center shrink-0 transition-all duration-300"
           >
@@ -78,7 +78,7 @@ export default function NavBar() {
           {/* Desktop Links */}
           <div className="hidden items-center gap-8 lg:flex">
             {navLinks.map((link) => (
-              
+              <a
                 key={link.label}
                 href={link.href}
                 target={link.href.startsWith("http") ? "_blank" : undefined}
@@ -129,7 +129,7 @@ export default function NavBar() {
             </div>
             <nav className="flex flex-col gap-1 p-4">
               {navLinks.map((link) => (
-                
+                <a
                   key={link.label}
                   href={link.href}
                   target={link.href.startsWith("http") ? "_blank" : undefined}
