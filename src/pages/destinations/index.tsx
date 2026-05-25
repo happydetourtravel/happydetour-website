a7612f70aaa02aae4aa83531c2e97c96ce0ce759
happydetourtravel <happydetourtravel@gmail.com>
2026-05-24 22:05:17 -0400

    Update index.tsx

--- src/pages/destinations/index.tsx
+++ src/pages/destinations/index.tsx
@@ -1,6 +1,6 @@
 import ScrollReveal from '../../components/base/ScrollReveal';
 
-const FORM_URL = "https://happydetour.app.n8n.cloud/form/91799171-4079-4ab7-9d13-9417ae57dfba";
+const FORM_URL = "https://api.leadconnectorhq.com/widget/form/V4qo3lEB8oTtXbuWILfK";
 const VIRGIN_URL = "https://www.virginvoyages.com/book/voyage-planner/find-a-voyage?cabins=1&currencyCode=USD&agentId=278796&agencyId=589&bookingChannel=FMLINK&priceType=cabinPerNight";
 const SANDALS_URL = "https://www.sandals.com/?referral=101731&agentid=ERCA7707";
 const BEACHES_URL = "https://www.beaches.com/?referral=101731&agentid=ERCA7707";
